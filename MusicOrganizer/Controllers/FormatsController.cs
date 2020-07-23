55ac7883ceb68c634332defa8cf9497307c56b25
JohnNils Olson <johnnils@gmail.com>
2020-07-23 11:39:22 -0700

    add routes
    Co-Authored-By: Chris Yoon <chy.yoon@gmail.com>

--- MusicOrganizer/Controllers/FormatsController.cs
+++ MusicOrganizer/Controllers/FormatsController.cs
@@ -1,9 +1,53 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using MusicOrganizer.Models;
 
 namespace MusicOrganizer.Controllers
 {
   public class FormatsController : Controller
   {
+    [HttpGet("/formats")]
+    public ActionResult Index()
+    {
+      List<Format> allformats = Format.GetAll();
+      return View(allformats);
+    }
     
+    [HttpGet("/formats/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/formats")]
+    public ActionResult Create(string formatType)
+    {
+      Format newFormat = new Format(formatType);
+      return RedirectToAction("Index");
+    }
+    
+    [HttpGet("/formats/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Format selectedFormat = Format.Find(id);
+      List<Record> formatRecords = selectedFormat.Records;
+      model.Add("format", selectedFormat);
+      model.Add("records", formatRecords);
+      return View(model);
+    }
+    [HttpPost("/formats/{formatId}/records")]
+    public ActionResult Create(int formatId, string recordTitle, string recordArtist)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Format foundFormat = Format.Find(formatId);
+      Record newRecord = new Record(recordTitle, recordArtist);
+      foundFormat.AddRecord(newRecord);
+      List<Record> formatRecords = foundFormat.Records;
+      model.Add("records", formatRecords);
+      model.Add("format", foundFormat);
+      return View("Show", model);
+    }
   }
 }
--- MusicOrganizer/Controllers/Records.Controller.cs
+++ MusicOrganizer/Controllers/Records.Controller.cs
@@ -1,9 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
+using MusicOrganizer.Models;
+using System.Collections.Generic;
 
 namespace MusicOrganizer.Controllers
 {
   public class RecordsController : Controller
   {
-    
+    [HttpGet("/formats/{formatId}/records/new")]
+    public ActionResult New(int formatId)
+    {
+      Format format = Format.Find(formatId);
+      return View(format);
+    }
+
+    [HttpGet("/formats/{formatId}/records/{recordId}")]
+    public ActionResult Show(int formatId, int recordId)
+    {
+      Record record = Record.Find(recordId);
+      Format format = Format.Find(formatId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("record", record);
+      model.Add("format", format);
+      return View(model);
+    }
+
+    [HttpPost("/records/delete")]
+    public ActionResult DeleteAll()
+    {
+      Record.ClearAll();
+      return View();
+    }
   }
 }
